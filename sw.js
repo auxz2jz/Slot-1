98d9d7614f4d052ac6c5f3fc4aef834950a5729f
auxz2jz <41704988+auxz2jz@users.noreply.github.com>
2026-09-11 01:13:37 -0700

    Bump cache for final footstep and bomb visual fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='emberwood-v36-r3';
+const CACHE='emberwood-v36-r4';
 const CORE=['./','./index.html','./core.js','./state.js','./stateRecovery.js','./upgrade.js','./systems.js','./expansion.js','./dungeonplus.js','./actionplus.js','./finaldungeon.js','./worldplus.js','./trials.js','./adventureplus.js','./dungeonPuzzles.js','./render.js','./renderRecovery.js','./effectRecovery.js','./graphics32.js','./graphics32plus.js','./graphics32action.js','./graphicsHD.js','./graphicsFinal.js','./graphicsPolish.js','./graphicsWorldplus.js','./graphicsTrials.js','./graphicsAdventure.js','./graphicsPuzzles.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
